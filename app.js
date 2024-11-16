024c14c2825f3b09a14804101c4f1f75a3df1abf
TheChec <chechoosorio@hotmail.com>
2024-11-15 21:27:54 -0500

    scroll reveal

--- app.js
+++ app.js
@@ -1,4 +1,38 @@
+window.sr = ScrollReveal();
+
 function goTo(){
     window.location.replace('./temary/temary.html')
 }
 
+sr.reveal('.portada',{
+    duration: 2000,
+    origin: 'bottom',
+    distance: '-150px',
+    reset: true
+})
+
+sr.reveal('.sectionCenter',{
+    duration: 2000,
+    origin: 'top',
+    distance: '-150px',
+    reset: true
+})
+
+sr.reveal('.sectionBg',{
+    duration: 2000,
+    origin: 'top',
+    distance: '-150px',
+    reset: true
+})
+sr.reveal('.textDivide',{
+    duration: 2000,
+    origin: 'top',
+    distance: '-150px',
+    reset: true
+})
+sr.reveal('.opiniones',{
+    duration: 2000,
+    origin: 'top',
+    distance: '-150px',
+    reset: true
+})
---
+++ temary/temary.js
@@ -0,0 +1,15 @@
+window.sr = ScrollReveal();
+
+sr.reveal('.portada',{
+    duration: 2000,
+    origin: 'bottom',
+    distance: '-150px',
+    reset: true
+})
+
+sr.reveal('.textDivide',{
+    duration: 2000,
+    origin: 'top',
+    distance: '-150px',
+    reset: true
+})
